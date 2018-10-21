1b3a23cf45ee35d9d0faf5d2fd4dd72368cfe695
bgs <bgs@benner.com.br>
2018-10-21 19:17:35 -0300

    ajuste readme

--- alb.tf
+++ alb.tf
@@ -18,7 +18,7 @@ resource "aws_alb_target_group" "app" {
   target_type = "ip"
 }
 
-# Redirect all traffic from the ALB to the target group
+
 resource "aws_alb_listener" "front_end" {
   load_balancer_arn = "${aws_alb.bgs_alb.id}"
   port              = "80"
